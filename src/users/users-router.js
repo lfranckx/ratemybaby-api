f9e3a4ca547eff1eb15ba567106959d348e34ca3
Bowie <bowie@thinkful.com>
2020-03-08 18:25:09 -0700

    update usersRouter

--- src/users/users-router.js
+++ src/users/users-router.js
@@ -1,155 +1,192 @@
 const path = require('path')
 const express = require('express')
-const xss = require('xss')
 const UsersService = require('./users-service')
-const bcrypt = require('bcrypt')
 const usersRouter = express.Router()
 const jsonParser = express.json()
 
-const serializeUser = user => ({
-    id: user.id,
-    username: xss(user.username),
-    user_password: xss(user.user_password),
-    email: xss(user.email),
-    baby_id: user.baby_id
-})
-
 usersRouter
-    .route('/users')
-    .get((req, res, next) => {
-        const knexInstance = req.app.get('db')
-        UsersService.getAllUsers(knexInstance)
-        .then(users => {
-            res.json(users.map(serializeUser))
-        })
-        .catch(next)
-    })
-    .post(jsonParser, (req, res, next) => {
+    .post('/', jsonParser, (req, res, next) => {
         const { username, user_password, email } = req.body
-        const newUser = { username, user_password, email }
-        // validate values are in req.body
-        for (const [key, value] of Object.entries(newUser))
-            if (value == null) 
+
+        for (const field of ['username', 'user_password', 'email'])
+            if(!req.body[field])
                 return res.status(400).json({
-                    error: { message: `Missing '${key}' in request body`}
+                    error: `Missing '${field} in request body`
                 })
 
-        UsersService.addNewUser(
-            req.app.get('db'),
-            newUser
-        )
-        .then(user => {
-            res
-            .status(201)
-            .location(path.posix.join(req.originalUrl, `/${user.id}`))
-            .json(serializeUser(user))
-        })
-        .catch(next)
-    })
+            const passwordError = UsersService.validatePassword
+            if (passwordError)
+                return res.status(400).json({ error: passwordError })
 
-usersRouter
-    .route('/users/:user_id')
-    .all((req, res, next) => {
-        UsersService.getUserById(
-            req.app.get('db'),
-            req.params.user_id
-        )
-        .then(user => {
-            if (!user) {
-                return res.status(404).json({
-                    error: { message: `User doesn't exist` }
-                })
-            }
-            res.user = user
-            next()
-        })
-        .catch(next)
-    })
-    .get((req, res, next) => {
-        res.json(serializeUser(res.user))
-    })
-    .delete((req, res, next) => {
-        UsersService.deleteUser(
-            req.app.get('db'),
-            req.params.user_id
-        )
-        .then(numRowsAffected => {
-            res.status(204).end()
-        })
-        .catch(next)
-    })
-    .patch(jsonParser, (req, res, next) => {
-        const { username, user_password, email } = req.body
-        const userToUpdate = { username, user_password, email }
-        const numOfValues = Object.values(userToUpdate).filter(Boolean).length
-        if (numOfValues === 0)
-            return res.status(400).json({
-                error: {
-                    message: `Request body must contain username, user_password, or email`
-                }
-            })
-        UsersService.updateUser(
-            req.app.get('db'),
-            req.params.user_id,
-            userToUpdate
-        )
-            .then(numRowsAffected => {
-                res.status(204).end()
-            })
-            .catch(next)
-    })
+                UsersService.hasUserWithUserName(
+                    req.app.get('db'),
+                    username
+                )
 
-usersRouter
-    .route('/login/:username')
-    .all((req, res, next) => {
-        UsersService.getUserByUsername(
-            req.app.get('db'),
-            req.params.username
-        )
-        .then(user => {
-            if (!user) {
-                return res.status(404).json({
-                    error: { message: `User doesn't exist` }
-                })
-            }
-            res.user = user
-            next()
-        })
-        .catch(next)
+                    .then(hasUserWithUserName => {
+                        if (hasUserWithUserName)
+                            return res.status(400).json({ error: `Username already taken` })
+
+                            return UsersService.hashPassword(password)
+                            .then(hashedPassword => {
+                                const newUser = {
+                                    username,
+                                    user_password: hashedPassword,
+                                    email,
+                                    date_created: 'now()'
+                                }
+
+                                return UsersService.insertUser(
+                                    req.app.get('db'),
+                                    newUser
+                                )
+                                .then(user => {
+                                    res
+                                        .status(201)
+                                        .location(path.posix.join(req.originalUrl, `/${user.id}`))
+                                        .json(UsersService.serializeUser(user))
+                                })
+                            })
+                    })
+                    .catch(next)
     })
-    .get((req, res, next) => {
-        res.json(serializeUser(res.user))
-    })
-    .delete((req, res, next) => {
-        UsersService.deleteUser(
-            req.app.get('db'),
-            req.params.username
-        )
-        .then(numRowsAffected => {
-            res.status(204).end()
-        })
-        .catch(next)
-    })
-    .patch(jsonParser, (req, res, next) => {
-        const { username, user_password, email } = req.body
-        const userToUpdate = { username, user_password, email }
-        const numOfValues = Object.values(userToUpdate).filter(Boolean).length
-        if (numOfValues === 0)
-            return res.status(400).json({
-                error: {
-                    message: `Request body must contain username, user_password, or email`
-                }
-            })
-        UsersService.updateUser(
-            req.app.get('db'),
-            req.params.username,
-            userToUpdate
-        )
-            .then(numRowsAffected => {
-                res.status(204).end()
-            })
-            .catch(next)
-    }) 
-    
     
 module.exports = usersRouter;
+
+// usersRouter
+//     .route('/users')
+//     .get((req, res, next) => {
+//         const knexInstance = req.app.get('db')
+//         UsersService.getAllUsers(knexInstance)
+//         .then(users => {
+//             res.json(users.map(serializeUser))
+//         })
+//         .catch(next)
+//     })
+//     .post(jsonParser, (req, res, next) => {
+//         const { username, user_password, email } = req.body
+//         const newUser = { username, user_password, email }
+//         // validate values are in req.body
+//         for (const [key, value] of Object.entries(newUser))
+//             if (value == null) 
+//                 return res.status(400).json({
+//                     error: { message: `Missing '${key}' in request body`}
+//                 })
+
+//         UsersService.addNewUser(
+//             req.app.get('db'),
+//             newUser
+//         )
+//         .then(user => {
+//             res
+//             .status(201)
+//             .location(path.posix.join(req.originalUrl, `/${user.id}`))
+//             .json(serializeUser(user))
+//         })
+//         .catch(next)
+//     })
+
+// usersRouter
+//     .route('/users/:user_id')
+//     .all((req, res, next) => {
+//         UsersService.getUserById(
+//             req.app.get('db'),
+//             req.params.user_id
+//         )
+//         .then(user => {
+//             if (!user) {
+//                 return res.status(404).json({
+//                     error: { message: `User doesn't exist` }
+//                 })
+//             }
+//             res.user = user
+//             next()
+//         })
+//         .catch(next)
+//     })
+//     .get((req, res, next) => {
+//         res.json(serializeUser(res.user))
+//     })
+//     .delete((req, res, next) => {
+//         UsersService.deleteUser(
+//             req.app.get('db'),
+//             req.params.user_id
+//         )
+//         .then(numRowsAffected => {
+//             res.status(204).end()
+//         })
+//         .catch(next)
+//     })
+//     .patch(jsonParser, (req, res, next) => {
+//         const { username, user_password, email } = req.body
+//         const userToUpdate = { username, user_password, email }
+//         const numOfValues = Object.values(userToUpdate).filter(Boolean).length
+//         if (numOfValues === 0)
+//             return res.status(400).json({
+//                 error: {
+//                     message: `Request body must contain username, user_password, or email`
+//                 }
+//             })
+//         UsersService.updateUser(
+//             req.app.get('db'),
+//             req.params.user_id,
+//             userToUpdate
+//         )
+//             .then(numRowsAffected => {
+//                 res.status(204).end()
+//             })
+//             .catch(next)
+//     })
+
+// usersRouter
+//     .route('/login/:username')
+//     .all((req, res, next) => {
+//         UsersService.getUserByUsername(
+//             req.app.get('db'),
+//             req.params.username
+//         )
+//         .then(user => {
+//             if (!user) {
+//                 return res.status(404).json({
+//                     error: { message: `User doesn't exist` }
+//                 })
+//             }
+//             res.user = user
+//             next()
+//         })
+//         .catch(next)
+//     })
+//     .get((req, res, next) => {
+//         res.json(serializeUser(res.user))
+//     })
+//     .delete((req, res, next) => {
+//         UsersService.deleteUser(
+//             req.app.get('db'),
+//             req.params.username
+//         )
+//         .then(numRowsAffected => {
+//             res.status(204).end()
+//         })
+//         .catch(next)
+//     })
+//     .patch(jsonParser, (req, res, next) => {
+//         const { username, user_password, email } = req.body
+//         const userToUpdate = { username, user_password, email }
+//         const numOfValues = Object.values(userToUpdate).filter(Boolean).length
+//         if (numOfValues === 0)
+//             return res.status(400).json({
+//                 error: {
+//                     message: `Request body must contain username, user_password, or email`
+//                 }
+//             })
+//         UsersService.updateUser(
+//             req.app.get('db'),
+//             req.params.username,
+//             userToUpdate
+//         )
+//             .then(numRowsAffected => {
+//                 res.status(204).end()
+//             })
+//             .catch(next)
+//     }) 
+    
--- src/users/users-service.js
+++ src/users/users-service.js
@@ -1,44 +1,92 @@
+const bcrypt = require('bcryptjs')
+const xss = require('xss')
+const REGEX_UPPER_LOWER_NUMBER_SPECIAL = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])[\S]+/
+
 const UsersService = {
-    getAllUsers(knex) {
-        return knex.select('*').from('users')
+    hasUserWithUserName(db, username) {
+        return db('users')
+            .where({ username })
+            .first()
+            .then(user => !!user)
     },
-    addNewUser(knex, newUser) {
-        return knex
+    insertUser(db, newUser) {
+        return db
             .insert(newUser)
             .into('users')
             .returning('*')
-            .then(rows => {
-                return rows[0]
-            })
+            .then(([user]) => user)
     },
-    getUserById(knex, id) {
-        return knex
-            .from('users')
-            .select('*')
-            .where('id', id).first()
+    validatePassword(password) {
+        if (password.length < 8) {
+            return 'Password must be longer than 8 characters'
+        }
+        if (password.length > 72) {
+            return 'Password must be less than 72 characters'
+        }
+        if (password.startsWith(' ') || password.endsWith(' ')) {
+            return 'Password must not start or end with empty spaces'
+        }
+        if (!REGEX_UPPER_LOWER_NUMBER_SPECIAL.test(password)) {
+            return `Password must contain 1 upper case, lower case, number and special character`
+        }
+        return null
     },
-    getUserByEmail(knex, email) {
-        return knex
-            .from('users')
-            .select('*')
-            .where('email', email).first()
+    hashPassword(password) {
+        return bcrypt.hash(password, 12)
     },
-    getUserByUsername(knex, username) {
-        return knex 
-            .from('users')
-            .select('*')
-            .where('username', username).first()
+    serializeUser(user) {
+        return {
+            id: user.id,
+            username: xss(user.username),
+            user_password: xss(user.user_password),
+            email: xss(user.email),
+            date_created: new Date(user.date_created),
+        }
     },
-    deleteUser(knex, id) {
-        return knex('users')
-            .where({ id })
-            .delete()
-    },
-    updateUser(knex, id, newUserFields) {
-        return knex('users')
-            .where({ id })
-            .update(newUserFields)
-    }
 }
 
 module.exports = UsersService
+
+
+// const UsersService = {
+//     getAllUsers(knex) {
+//         return knex.select('*').from('users')
+//     },
+//     addNewUser(knex, newUser) {
+//         return knex
+//             .insert(newUser)
+//             .into('users')
+//             .returning('*')
+//             .then(rows => {
+//                 return rows[0]
+//             })
+//     },
+//     getUserById(knex, id) {
+//         return knex
+//             .from('users')
+//             .select('*')
+//             .where('id', id).first()
+//     },
+//     getUserByEmail(knex, email) {
+//         return knex
+//             .from('users')
+//             .select('*')
+//             .where('email', email).first()
+//     },
+//     getUserByUsername(knex, username) {
+//         return knex 
+//             .from('users')
+//             .select('*')
+//             .where('username', username).first()
+//     },
+//     deleteUser(knex, id) {
+//         return knex('users')
+//             .where({ id })
+//             .delete()
+//     },
+//     updateUser(knex, id, newUserFields) {
+//         return knex('users')
+//             .where({ id })
+//             .update(newUserFields)
+//     }
+// }
