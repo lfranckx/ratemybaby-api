c9f8905616ab43f212636e79e7b8c1b757782bd5
Bowie <bowie@thinkful.com>
2020-03-25 17:37:11 -0700

    update

--- seeds/seeds.ratemybaby_tables.sql
+++ seeds/seeds.ratemybaby_tables.sql
@@ -23,7 +23,7 @@ VALUES
     (
         'Noah', 
         'Im baby edison bulb taxidermy jianbing you probably havent heard of them palo santo, next level vegan sriracha. Lumbersexual craft beer fanny pack chicharrones palo santo woke af hammock single-origin coffee raclette intelligentsia semiotics crucifix raw denim freegan. Yr 3 wolf moon tousled hexagon, trust fund vexillologist umami gastropub taxidermy salvia hot chicken snackwave street art ramps semiotics.',
-        '../../images/Noah.jpg',
+        'https://ratemybaby-images.s3-us-west-1.amazonaws.com/Noah.jpg',
         16,
         20,
         1
@@ -31,7 +31,7 @@ VALUES
     (
         'Emma', 
         'Tousled quinoa whatever letterpress kickstarter 90s. Marfa skateboard cornhole tousled twee slow-carb. Forage cold-pressed asymmetrical pop-up bicycle rights small batch synth. Flannel cronut gentrify pickled, tote bag jianbing pabst fashion axe. Lumbersexual franzen forage helvetica pinterest, typewriter squid.',
-        '../../images/Emma.jpg',
+        'https://ratemybaby-images.s3-us-west-1.amazonaws.com/Emma.jpg',
         15,
         20,
         2
@@ -39,7 +39,7 @@ VALUES
     (
         'Frank', 
         'Raw denim ennui edison bulb woke health goth. Austin mustache try-hard gluten-free, flannel squid copper mug. Fashion axe church-key iPhone, selvage slow-carb waistcoat tilde. Celiac blue bottle hell of, before they sold out yr kinfolk knausgaard cornhole helvetica salvia mixtape microdosing hexagon.',
-        '../../images/Frank.jpg',
+        'https://ratemybaby-images.s3-us-west-1.amazonaws.com/Frank.jpg',
         10,
         20,
         3
@@ -47,7 +47,7 @@ VALUES
     (
         'CJ', 
         'Migas crucifix adaptogen whatever live-edge tumeric godard iPhone typewriter coloring book raclette banh mi aesthetic vexillologist. Intelligentsia pug slow-carb, viral plaid butcher 3 wolf moon hammock kombucha irony. Cardigan gastropub four loko before they sold out, hexagon austin pop-up venmo forage chartreuse bicycle rights tumblr taxidermy green juice biodiesel.',
-        '../../images/CJ.jpg',
+        'https://ratemybaby-images.s3-us-west-1.amazonaws.com/CJ.jpg',
         16,
         20,
         4
@@ -55,7 +55,7 @@ VALUES
     (
         'Kylie', 
         'Listicle roof party quinoa, brunch meditation waistcoat iceland poke post-ironic taiyaki. YOLO flexitarian hashtag mustache edison bulb, la croix whatever. Actually stumptown drinking vinegar, occupy leggings af cloud bread salvia cray hoodie jean shorts distillery williamsburg single-origin coffee try-hard. Stumptown sustainable skateboard la croix tofu ugh pug yuccie church-key knausgaard gastropub.',
-        '../../images/Kylie.jpg',
+        'https://ratemybaby-images.s3-us-west-1.amazonaws.com/Kylie.jpg',
         9,
         20,
         5
@@ -63,7 +63,7 @@ VALUES
     (
         'Stephen', 
         'Tumblr franzen schlitz quinoa coloring book unicorn blue bottle hell of godard mustache kitsch fingerstache seitan edison bulb. Glossier iPhone leggings, flexitarian organic butcher polaroid cliche. Schlitz try-hard viral keytar intelligentsia unicorn.',
-        '../../images/Stephen.jpg',
+        'https://ratemybaby-images.s3-us-west-1.amazonaws.com/Stephen.jpg',
         8,
         20,
         6
@@ -71,7 +71,7 @@ VALUES
     (
         'Lauren', 
         'Marfa banh mi health goth, unicorn palo santo tattooed typewriter jean shorts hexagon taiyaki raw denim tousled master cleanse af crucifix. 3 wolf moon vice sartorial quinoa. Letterpress literally messenger bag banjo cold-pressed. Normcore ugh kogi vape irony, lyft raw denim thundercats neutra coloring book twee. Tbh tote bag irony cliche biodiesel beard semiotics banjo.',
-        '../../images/Lauren.jpg',
+        'https://ratemybaby-images.s3-us-west-1.amazonaws.com/Lauren.jpg',
         17,
         20,
         7
@@ -79,15 +79,15 @@ VALUES
     (
         'Dan', 
         'Gluten-free biodiesel mustache coloring book kinfolk wayfarers, helvetica gochujang put a bird on it jean shorts tumeric marfa photo booth squid. Ennui bicycle rights irony echo park scenester YOLO drinking vinegar street art shoreditch.',
-        '../../images/Dan.jpg',
+        'https://ratemybaby-images.s3-us-west-1.amazonaws.com/Dan.jpg',
         5,
         20,
         8
     ),
     (
-        'Jon', 
+        'John', 
         'I''m baby pBR&B kickstarter put a bird on it af 3 wolf moon viral la croix keffiyeh meggings dreamcatcher hoodie microdosing you probably haven''t heard of them. Humblebrag pitchfork stumptown coloring book butcher air plant. Snackwave occupy flexitarian, biodiesel beard subway tile blog gluten-free synth knausgaard schlitz VHS.',
-        '../../images/Jon.jpg',
+        'https://ratemybaby-images.s3-us-west-1.amazonaws.com/John.jpg',
         10,
         20,
         9
@@ -95,7 +95,7 @@ VALUES
     (
         'Danielle', 
         'Selfies keytar leggings, fashion axe chia pitchfork mixtape. Hashtag DIY hoodie, woke vape distillery XOXO organic. Green juice stumptown skateboard fam, air plant wolf vinyl twee meditation intelligentsia. Chartreuse coloring book vexillologist, listicle photo booth kinfolk art party chambray lyft hoodie.',
-        '../../images/Danielle.jpg',
+        'https://ratemybaby-images.s3-us-west-1.amazonaws.com/Danielle.jpg',
         7,
         20,
         10
