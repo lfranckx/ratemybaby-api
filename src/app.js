dd09600aa4a7478cc1079488b6bac8c1d59fd8a1
Bowie <bowie@thinkful.com>
2020-04-07 17:44:52 -0700

    updating for deployment

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const { CLIENT_ORIGIN } = require('./config')
 
 const app = express()
 const morganOption = (NODE_ENV === 'production')
@@ -20,6 +21,9 @@ app.get('/', (req, res) => {
   res.send('Hello, boilerplate!')
 })
 
+app.use(cors({
+  origin:CLIENT_ORIGIN
+}))
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
